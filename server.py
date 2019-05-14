da7558cc1ec065532d40e34cbc37211d1b8bf91f
ysir <577790911@qq.com>
2019-05-14 11:09:26 +0800

    a

--- server.py
+++ server.py
@@ -4,13 +4,11 @@
 from flask import Flask
 from jinja2 import Markup, Environment, FileSystemLoader
 from pyecharts.globals import CurrentConfig
-
-# 关于 CurrentConfig，可参考 [基本使用-全局变量]
-CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./templates"))
-
 from pyecharts import options as opts
 from pyecharts.charts import Bar
 
+# 关于 CurrentConfig，可参考 [基本使用-全局变量]
+CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./templates"))
 
 app = Flask(__name__, static_folder="templates")
 
@@ -18,11 +16,12 @@ app = Flask(__name__, static_folder="templates")
 def bar_base() -> Bar:
     c = (
         Bar()
-        .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-        .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+            .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
     )
+
     return c
 
 
@@ -33,4 +32,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
