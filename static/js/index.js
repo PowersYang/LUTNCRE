f0817b0c1b84bbc5c0a7f44fe7fbea00a43134d9
Y-sir <577790911@qq.com>
2019-05-17 00:06:34 +0800

    添加地图

---
+++ static/js/index.js
@@ -0,0 +1,30 @@
+$(function () {
+    //点击跳转
+    $('#chart_map').click(function(){
+        window.location.href = '../../templates/index2.html';
+    });
+    $('.t_btn2').click(function(){
+        window.location.href = "../../templates/index2.html?id=2";
+    });
+    $('.t_btn3').click(function(){
+        window.location.href = "../../templates/index2.html?id=3";
+    });
+    $('.t_btn4').click(function(){
+        window.location.href = "../../templates/index2.html?id=4";
+    });
+    $('.t_btn5').click(function(){
+        window.location.href = "../../templates/index2.html?id=5";
+    });
+    $('.t_btn6').click(function(){
+        window.location.href = "../../templates/index2.html?id=6";
+    });
+    $('.t_btn7').click(function(){
+        window.location.href = "../../templates/index2.html?id=7";
+    });
+    $('.t_btn8').click(function(){
+        window.location.href = "../../templates/index2.html?id=8";
+    });
+    $('.t_btn9').click(function(){
+        window.location.href = "../../templates/index2.html?id=9";
+    });
+});
