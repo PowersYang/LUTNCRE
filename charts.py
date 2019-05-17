8572a20d1723f7a3a3a9c653d20143a608332477
Y-sir <577790911@qq.com>
2019-05-18 00:43:10 +0800

    :art:update

--- charts.py
+++ charts.py
@@ -5,18 +5,25 @@ from pyecharts import options as opts
 from pyecharts.globals import ChartType, ThemeType
 
 
-def pass_rate_bar(date, data, title, width, height):
+def pass_rate_bar(date=None, data=None, title=None, width=None, height=None, reverse=False):
     c = (
-        Bar(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height),
-                                    theme=ThemeType.MACARONS))
+        Bar(init_opts=opts.InitOpts(
+            width="{}px".format(width), height="{}px".format(height),
+        ))
             .add_xaxis(list(data.index))
-            .set_global_opts(
-            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-90)),
-            # title_opts=opts.TitleOpts(title=title)
-        ).add_yaxis(date, list(data.values))
+            .add_yaxis(date, list(data.values), color="#7dd0f3")
+            .set_global_opts(xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-30, color="#ffffff"),
+                                                      axisline_opts=opts.AxisLineOpts(
+                                                          linestyle_opts=opts.LineStyleOpts(color="#fff"))),
+                             yaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(color="#ffffff"),
+                                                      axisline_opts=opts.AxisLineOpts(
+                                                          linestyle_opts=opts.LineStyleOpts(color="#ffffff"))))
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-
+            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False))
     )
+
+    if reverse:
+        c.reversal_axis()
 
     return c
 
@@ -29,16 +36,16 @@ def geo_effectscatter(data, title, width, height) -> Geo:
             "各省通过率",
             [list(z) for z in zip(data.index, data.values)],
             type_=ChartType.EFFECT_SCATTER,
+            effect_opts=opts.EffectOpts(scale=1000)
         )
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-            # .set_global_opts(title_opts=opts.TitleOpts(title=title))
     )
     return c
 
 
 def gaugegauge_base_color(data, title, width, height) -> Gauge:
     c = (
-        Gauge(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height)))
+        Gauge(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height)), )
             .add(
             "整体通过率",
             [("通过率", data)],
@@ -48,10 +55,8 @@ def gaugegauge_base_color(data, title, width, height) -> Gauge:
                 )
             ),
         )
-            .set_global_opts(
-            # title_opts=opts.TitleOpts(title=title),
-            legend_opts=opts.LegendOpts(is_show=False),
-        )
+            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False),
+                             )
     )
     return c
 
@@ -62,25 +67,11 @@ def pie_rich_label(data, title, width, height) -> Pie:
             .add(
             "",
             [list(z) for z in zip(data.index, [int(a) for a in data.values])],
-            radius=["40%", "55%"],
-            label_opts=opts.LabelOpts(
-                position="outside",
-                formatter=" {b|{b}: }{c}  {per|{d}%}  ",
-                background_color="#eee",
-                border_color="#aaa",
-                border_width=1,
-                border_radius=4,
-                rich={
-                    "b": {"fontSize": 16, "lineHeight": 33},
-                    "per": {
-                        "color": "#eee",
-                        "backgroundColor": "#334455",
-                        "padding": [2, 4],
-                        "borderRadius": 2,
-                    },
-                },
-            ),
+            radius=["40%", "60%"],
+
         )
-            # .set_global_opts(title_opts=opts.TitleOpts(title=title))
+            .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False))
+
     )
     return c
--- server.py
+++ server.py
@@ -17,12 +17,13 @@ def index():
 def render_charts(date):
     analysis = Analysis(date)
 
-    c1 = pass_rate_bar(date, analysis.pass_rate('college'), '各学院通过率', 500, 300)
+    c1 = pass_rate_bar(date, analysis.pass_rate('college'), '各学院通过率', 600, 400)
     c2 = pass_rate_bar(date, analysis.pass_rate('major'), '各专业通过率', 500, 300)
-    c3 = pass_rate_bar(date, analysis.pass_rate('subject'), '各科目通过率', 500, 300)
-    c4 = pie_rich_label(analysis.pass_rate_fd(), '人数统计', 500, 300)
-    c5 = gaugegauge_base_color(analysis.pass_rate(), '整体通过率', 500, 300)
-    c6 = geo_effectscatter(analysis.pass_rate('jg'), '各省通过率', 500, 300)
+    c3 = pass_rate_bar(date, analysis.pass_rate('subject'), '各科目通过率', 300, 610, True)
+    c4 = pie_rich_label(analysis.pass_rate_fd(), '人数统计', 300, 300)
+    # c4 = pie_radius()
+    c5 = gaugegauge_base_color(analysis.pass_rate(), '整体通过率', 330, 330)
+    c6 = geo_effectscatter(analysis.pass_rate('jg'), '各省通过率', 600, 600)
 
     return render_template('pyecharts.html', c1=c1.render_embed(), c2=c2.render_embed(),
                            c3=c3.render_embed(), c4=c4.render_embed(),
