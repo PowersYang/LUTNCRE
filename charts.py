4732af8e3ff7e605d3b865e7ccf3c18546b6e112
Y-sir <577790911@qq.com>
2019-05-18 02:23:46 +0800

    :art:update

--- charts.py
+++ charts.py
@@ -5,45 +5,50 @@ from pyecharts import options as opts
 from pyecharts.globals import ChartType, ThemeType
 
 
-def pass_rate_bar(date=None, data=None, title=None, width=None, height=None, reverse=False):
+def pass_rate_bar(date=None, data=None, width=None, height=None, reverse=False, xrotate=None, yrorate=None,
+                  datazoom=False):
     c = (
         Bar(init_opts=opts.InitOpts(
             width="{}px".format(width), height="{}px".format(height),
         ))
             .add_xaxis(list(data.index))
             .add_yaxis(date, list(data.values), color="#7dd0f3")
-            .set_global_opts(xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-30, color="#ffffff"),
+            .set_global_opts(xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=xrotate, color="#ffffff"),
                                                       axisline_opts=opts.AxisLineOpts(
-                                                          linestyle_opts=opts.LineStyleOpts(color="#fff"))),
-                             yaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(color="#ffffff"),
+                                                          linestyle_opts=opts.LineStyleOpts(color="#ffffff"))),
+                             yaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=yrorate, color="#ffffff"),
                                                       axisline_opts=opts.AxisLineOpts(
-                                                          linestyle_opts=opts.LineStyleOpts(color="#ffffff"))))
+                                                          linestyle_opts=opts.LineStyleOpts(color="#ffffff"))),
+                             legend_opts=opts.LegendOpts(is_show=False))
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False))
     )
 
     if reverse:
         c.reversal_axis()
 
+    if datazoom:
+        c.set_global_opts(datazoom_opts=[opts.DataZoomOpts(type_='inside'), opts.DataZoomOpts()],
+                          legend_opts=opts.LegendOpts(is_show=False))
+
     return c
 
 
-def geo_effectscatter(data, title, width, height) -> Geo:
+def geo_effectscatter(data, width, height) -> Geo:
     c = (
         Geo(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height)))
-            .add_schema(maptype="china", is_roam=False)
-            .add(
-            "各省通过率",
-            [list(z) for z in zip(data.index, data.values)],
-            type_=ChartType.EFFECT_SCATTER,
-            effect_opts=opts.EffectOpts(scale=1000)
-        )
+            .add_schema(maptype="china", is_roam=False,
+                        itemstyle_opts=opts.ItemStyleOpts(color='#0f1c2f', border_color="#5399b4", border_color0='red'))
+            .add("各省通过率",
+                 [list(z) for z in zip(data.index, data.values)],
+                 type_=ChartType.EFFECT_SCATTER,
+                 effect_opts=opts.EffectOpts(scale=1000), color="#d0f551")
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False))
     )
     return c
 
 
-def gaugegauge_base_color(data, title, width, height) -> Gauge:
+def gaugegauge_base_color(data, width, height) -> Gauge:
     c = (
         Gauge(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height)), )
             .add(
@@ -61,7 +66,7 @@ def gaugegauge_base_color(data, title, width, height) -> Gauge:
     return c
 
 
-def pie_rich_label(data, title, width, height) -> Pie:
+def pie_rich_label(data, width, height) -> Pie:
     c = (
         Pie(init_opts=opts.InitOpts(width="{}px".format(width), height="{}px".format(height)))
             .add(
--- server.py
+++ server.py
@@ -17,13 +17,12 @@ def index():
 def render_charts(date):
     analysis = Analysis(date)
 
-    c1 = pass_rate_bar(date, analysis.pass_rate('college'), '各学院通过率', 600, 400)
-    c2 = pass_rate_bar(date, analysis.pass_rate('major'), '各专业通过率', 500, 300)
-    c3 = pass_rate_bar(date, analysis.pass_rate('subject'), '各科目通过率', 300, 610, True)
-    c4 = pie_rich_label(analysis.pass_rate_fd(), '人数统计', 300, 300)
-    # c4 = pie_radius()
-    c5 = gaugegauge_base_color(analysis.pass_rate(), '整体通过率', 330, 330)
-    c6 = geo_effectscatter(analysis.pass_rate('jg'), '各省通过率', 600, 600)
+    c1 = pass_rate_bar(date, analysis.pass_rate('college'), 680, 380, False, -20, 0)
+    c2 = pass_rate_bar(date, analysis.pass_rate('major'), 650, 380, False, -20, 0, True)
+    c3 = pass_rate_bar(date, analysis.pass_rate('subject'), 300, 610, True, 0, -20)
+    c4 = pie_rich_label(analysis.pass_rate_fd(), 300, 300)
+    c5 = gaugegauge_base_color(analysis.pass_rate(), 330, 330)
+    c6 = geo_effectscatter(analysis.pass_rate('jg'), 700, 700)
 
     return render_template('pyecharts.html', c1=c1.render_embed(), c2=c2.render_embed(),
                            c3=c3.render_embed(), c4=c4.render_embed(),
